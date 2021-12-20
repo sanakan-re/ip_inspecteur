1a0f5333f339d983c826d4bd7e62ff069b13db6b
sanakan-re <sanakan-re@protonmail.com>
2021-12-20 18:26:53 +0100

    Premier commit nothing special

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+extern crate serde_json;
+use serde::Deserialize;
+use serde_json::Value as JsonValue;
+use std::collections::HashMap;
+use reqwest::header::HeaderMap;
+
+// #[derive(Deserialize, Debug)]
+// struct Ip_intel {
+//     ip_sus: String
+// }
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let api_key = "31616d3c6fe801289c3db2730cf07cb35f682abf21a00c79136af88bdb3dd797";
+
+    let mut headers = HeaderMap::new();
+    headers.insert("x-apikey", format!("{}", api_key).parse().unwrap());
+
+    let client = reqwest::Client::new();
+    let resp = client
+        .get("https://www.virustotal.com/api/v3/ip_addresses/10.10.10.10")
+        .headers(headers)
+        .send()
+        //.json::<HashMap<String, String>>()
+        .await?;
+
+    println!("{:#?}", resp);
+
+    let resp_json = resp.json::<HashMap<String, JsonValue>>().await?;
+    // let v: JsonValue = serde_json::from_str(resp_json.as_slice().to_vec())?;
+    // let resp_jj = serde_json::from_str(resp_json);
+    // let data : JsonValue = resp_jj.unwrap();
+    println!("{:#?}", resp_json);
+
+    Ok(())
+}
+
+
