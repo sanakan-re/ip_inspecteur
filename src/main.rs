36a253b46d5a17d5b136048f9f41f4b9b8b1554f
sanakan-re <sanakan-re@protonmail.com>
2022-01-06 16:30:03 +0100

    ip option v0.1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate serde_json;
 use serde_json::Value as JsonValue;
 // use std::collections::HashMap;
 use reqwest::header::HeaderMap;
-use std::env;
+// use std::env;
 use std::process;
 use clap::Parser;
 use ip_inspecteur::Args;
@@ -19,13 +19,15 @@ fn main() {
 
     // let args: Vec<String> = env::args().collect();
     let args = Args::parse();
+
+    virust_req(&args.ip);
     
 //     let params = Params::new(&args).unwrap_or_else( |err | {
 //         println!("Problem parsing arguments: {}", err);
 //         process::exit(1);
 //     });
 
-    println!("IP adress: {}\n", args.ip);
+    // println!("IP adress: {}\n", args.ip);
 
     if let Err(e) = run(args) {
         println!("Application error: {}", e);
@@ -34,15 +36,18 @@ fn main() {
 }
 
 #[tokio::main]
-async fn virust_req() -> Result<(), Box<dyn std::error::Error>> {
+async fn virust_req(ip: &String) -> Result<(), Box<dyn std::error::Error>> {
     let api_key = "31616d3c6fe801289c3db2730cf07cb35f682abf21a00c79136af88bdb3dd797";
+    let vt_adress = String::from("https://www.virustotal.com/api/v3/ip_addresses/"); 
+
+    let _vt_adress_i = vt_adress + &ip;
 
     let mut headers = HeaderMap::new();
     headers.insert("x-apikey", format!("{}", api_key).parse().unwrap());
 
     let client = reqwest::Client::new();
     let resp = client
-        .get("https://www.virustotal.com/api/v3/ip_addresses/10.10.10.10")
+        .get(_vt_adress_i)
         .headers(headers)
         .send()
         //.json::<HashMap<String, String>>()
